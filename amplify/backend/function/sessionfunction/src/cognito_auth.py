1b8509e79053e46b765bc2fc2f41f15a6896dbe8
Santiago Perez <santiago.perez@Santiagos-MacBook-Pro.local>
2023-07-13 16:27:48 -0500

    Adding logic to session lambda function for signup, signin and confirm signup actions

---
+++ amplify/backend/function/sessionfunction/src/cognito_auth.py
@@ -0,0 +1,50 @@
+from flask import Blueprint, request, jsonify
+import boto3
+import os
+
+cognito_auth_bp = Blueprint('cognito_auth', __name__)
+
+client = boto3.client("cognito-idp", region_name=os.getenv('REGION'))
+client_id = os.getenv('AWS_COGNITO_CLIENT_ID')
+
+@cognito_auth_bp.route('/sign_in', methods=['POST'])
+def sign_in():
+  request_args = request.get_json()
+  username = request_args['email']
+  password = request_args['password']
+
+  initiate_auth_response = client.initiate_auth(
+    ClientId=client_id,
+    AuthFlow="USER_PASSWORD_AUTH",
+    AuthParameters={"USERNAME": username, "PASSWORD": password},
+  )
+
+  return jsonify(data=initiate_auth_response)
+
+@cognito_auth_bp.route('/sign_up', methods=['POST'])
+def sign_up():
+  request_args = request.get_json()
+  username = request_args['email']
+  password = request_args['password']
+
+  sign_up_response = client.sign_up(
+    ClientId=client_id,
+    Username=username,
+    Password=password
+  )
+  
+  return jsonify(data=sign_up_response)
+
+@cognito_auth_bp.route('/confirm_sign_up', methods=['POST'])
+def confirm_sign_up():
+  request_args = request.get_json()
+  username = request_args['email']
+  confirmation_code = request_args['confirmation_code']
+
+  confirm_sign_up_response = client.confirm_sign_up(
+    ClientId=client_id,
+    Username=username,
+    ConfirmationCode=confirmation_code,
+  )
+
+  return jsonify(data=confirm_sign_up_response)
--- amplify/backend/function/sessionfunction/src/index.py
+++ amplify/backend/function/sessionfunction/src/index.py
@@ -1,15 +1,17 @@
-import json
+import awsgi
+from flask_cors import CORS
+from flask import Flask
+from cognito_auth import cognito_auth_bp
+
+def create_app():
+  app = Flask(__name__)
+  CORS(app)
+  
+  # Blueprints
+  app.register_blueprint(cognito_auth_bp)
+
+  return app
 
 def handler(event, context):
-  print('received event:')
-  print(event)
-  
-  return {
-      'statusCode': 200,
-      'headers': {
-          'Access-Control-Allow-Headers': '*',
-          'Access-Control-Allow-Origin': '*',
-          'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
-      },
-      'body': json.dumps('Hello from your new Amplify Python lambda!')
-  }
+  app = create_app()
+  return awsgi.response(app, event, context)
